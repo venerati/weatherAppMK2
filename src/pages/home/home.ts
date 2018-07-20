f441a6911851fd187b2b7848b7a23c94b1849acb
Marshall <JMarshall@ECFMG.ORG>
2018-07-20 16:11:05 -0400

    app will now automatically pull the location from the gps  and then pull the weather information down and display it still issues with the phone not giving the app permissions need to figure out how to make it ask for permission

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { WeatherApiProvider } from '../../providers/weather-api/weather-api';
 import { Storage } from '@ionic/storage';
 import { Geolocation } from '@ionic-native/geolocation';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -26,9 +27,11 @@ export class HomePage {
     //check if there is a stored location, if there is then make a call and get the weather info.
 
     //if there is not a stored location then call your gps location then use that lat long to get weather info.
-    this.initialGPSCheck()
-    this.clickGetForecast(this.lat,this.long);
-    this.getCityNameFromLL(this.lat,this.long);
+    
+  }
+
+  ngAfterViewInit(){
+    this.weatherSearchFromCurrentGPS()
   }
 
   //fires the request for forcast info that will be displayed to the user.
@@ -43,16 +46,14 @@ export class HomePage {
     });
   };
 
-  initialGPSCheck(){
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        console.log("navigator.geolocation works well");
-        this.storeCurrentLocationGPS()
-        this.getLatLongFromStorage()
-    }
-  }
+  // initialGPSCheck() {
+  //   console.log("navigator.geolocation works well");
+  //   this.storeCurrentLocationGPS()
+  //   this.getLatLongFromStorage()
+    
+  // }
 
-  //I don think this api is going to work for looking up the city from lat long, no real standard to the response.
+  //I dont think this api is going to work for looking up the city from lat long, no real standard to the response.
   getCityNameFromLL(lat,long){
     let location = lat+","+long;
     this.weatherService.getGeocodeApi(location).subscribe(res => {
@@ -82,45 +83,78 @@ export class HomePage {
   }
 
   //this method uses the phones's gps to grab the current location then sets those values into storage for later use.
-  storeCurrentLocationGPS(){
-    console.log('storecurrentlocation has fired')
+  // storeCurrentLocationGPS(){
+  //   console.log('storecurrentlocation has fired')
+  //   this.geolocation.getCurrentPosition().then((res) => {
+  //     console.log('the gps resp is ' + res.coords.latitude);
+  //     console.log('the gps resp is ' + res.coords.longitude);
+  //     this.storage.set('lat', res.coords.latitude);
+  //     this.storage.set('long', res.coords.longitude)
+  //    }).catch((error) => {
+  //      console.log('Error getting location', error);
+  //    });
+  // }
+
+  weatherSearchFromCurrentGPS(){
     this.geolocation.getCurrentPosition().then((res) => {
       console.log('the gps resp is ' + res.coords.latitude);
       console.log('the gps resp is ' + res.coords.longitude);
-      this.storage.set('lat', res.coords.latitude);
-      this.storage.set('long', res.coords.longitude)
+      this.lat = res.coords.latitude
+      this.long = res.coords.longitude
+      this.clickGetForecast(this.lat,this.long);
+      this.getCityNameFromLL(this.lat,this.long);
      }).catch((error) => {
        console.log('Error getting location', error);
      });
   }
 
-  getLatLongFromStorage(){
-    console.log('getlatlongfromstorage has fired')
-    this.storage.get('lat').then((res) => {
-      this.lat = res;
-    });
+  //this is used to set the lat long variables with the data set in app storage.
+  // getLatLongFromStorage(){
+  //   console.log('getlatlongfromstorage has fired')
+  //   this.storage.get('lat').then((res) => {
+  //     this.lat = res;
+  //     console.log(this.lat)
+  //   });
 
-    this.storage.get('long').then((res) => {
-      this.long = res;
-    })
-    console.log(this.lat + this.long)
-  }
+  //   this.storage.get('long').then((res) => {
+  //     this.long = res;
+  //     console.log(this.long)
+  //   })
+    
+  // }
 
   //grab the user input from a text field and set it to a var
-  setLocation(){
-    this.userProvidedLocation = (<HTMLInputElement>document.getElementById("locationInput")).value;
-    console.log(this.userProvidedLocation);
+  setLocation(): Observable<any> {
+    return new Observable(observer => {
+      let location = (<HTMLInputElement>document.getElementById("locationInput")).value
+      console.log(location);
+      //this.userProvidedLocation = location
+      observer.next(location)
+    })
+    
+    
+  }
+
+  searchFromUserInput(){
+    this.setLocation().subscribe(res => {
+      this.clickgetGeocodeAPI(res).subscribe(res => {
+        this.clickGetForecast(this.lat,this.long);
+        this.getCityNameFromLL(this.lat,this.long);
+      });
+    })
   }
 
   //this takes the location provided by the user and then returns the lat long info that is then bound to the local vars for use in the getforecast().
-  clickgetGeocodeAPI(location){
-    this.weatherService.getGeocodeApi(location).subscribe(res =>{
-      console.log(res);
-      let parsedRes = JSON.parse(res);
-      this.lat = parsedRes.results[0].geometry.location.lat;
-      this.long = parsedRes.results[0].geometry.location.lng;
-      console.log(this.lat);
-    });
+  clickgetGeocodeAPI(location): Observable<any>{
+    return new Observable(observer => {
+      this.weatherService.getGeocodeApi(location).subscribe(res =>{
+        console.log(res);
+        let parsedRes = JSON.parse(res);
+        this.lat = parsedRes.results[0].geometry.location.lat;
+        this.long = parsedRes.results[0].geometry.location.lng;
+        observer.next(res)
+      });
+    })
   };
 
 }
